3df30e259fc03f1e22909168b0152404589faf5a
Kanersps <kanepetra@gmail.com>
2017-02-24 13:36:44 +0100

    initial commit

---
+++ client/main.lua
@@ -0,0 +1,90 @@
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsSessionStarted() then
+			TriggerServerEvent('es:firstJoinProper')
+			return
+		end
+	end
+end)
+
+local loaded = false
+local cashy = 0
+local oldPos
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(3000)
+		local pos = GetEntityCoords(GetPlayerPed(-1))
+
+		if(oldPos ~= pos)then
+			TriggerServerEvent('es:updatePositions', pos.x, pos.y, pos.z)
+
+			if(loaded)then
+				SendNUIMessage({
+					setmoney = true,
+					money = cashy
+				})
+
+				loaded = false
+			end
+			oldPos = pos
+		end
+	end
+end)
+
+RegisterNetEvent('es:activateMoney')
+AddEventHandler('es:activateMoney', function(e)
+	SendNUIMessage({
+		setmoney = true,
+		money = e
+	})
+end)
+
+RegisterNetEvent("es:addedMoney")
+AddEventHandler("es:addedMoney", function(m)
+	SendNUIMessage({
+		addcash = true,
+		money = m
+	})
+
+end)
+
+RegisterNetEvent("es:removedMoney")
+AddEventHandler("es:removedMoney", function(m)
+	SendNUIMessage({
+		removecash = true,
+		money = m
+	})
+end)
+
+RegisterNetEvent("es:setMoneyDisplay")
+AddEventHandler("es:setMoneyDisplay", function(val)
+	SendNUIMessage({
+		setDisplay = true,
+		display = val
+	})
+end)
+
+RegisterNetEvent("es:enablePvp")
+AddEventHandler("es:enablePvp", function()
+	Citizen.CreateThread(function()
+		while true do
+			Citizen.Wait(0)
+			for i = 0,32 do
+				if NetworkIsPlayerConnected(i) then
+					if NetworkIsPlayerConnected(i) and GetPlayerPed(i) ~= nil then
+						SetCanAttackFriendly(GetPlayerPed(i), true, true)
+						NetworkSetFriendlyFireOption(true)
+					end
+				end
+			end
+		end
+	end)
+end)
